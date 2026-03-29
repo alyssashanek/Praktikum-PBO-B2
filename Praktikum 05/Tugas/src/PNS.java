797a73a06620afc3432e708914a984a57d874866
timiie <alyssashane2607@gmail.com>
2026-03-29 23:59:36 +0700

    Abstract Class & Interface

---
+++ Praktikum 05/Tugas/src/PNS.java
@@ -0,0 +1,47 @@
+import java.time.LocalDate;
+
+public class PNS extends Manusia implements Pajak {
+    private String nip;
+    private static int counterPNS = 0;
+
+    public PNS(String nama, LocalDate tgl, String alamat, double pendapatan, String nip){
+        super(nama, tgl, alamat, pendapatan);
+        this.nip = nip;
+        counterPNS++;
+    }
+
+    public PNS(String nama, LocalDate tgl, double pendapatan, String nip){
+        super(nama, tgl, "-", pendapatan);
+        this.nip = nip;
+        counterPNS++;
+    }
+
+    public String getNip(){
+        return nip;
+    }
+
+    public void setNip(String nip){
+        this.nip = nip;
+    }
+
+    public static int getCounterPNS(){
+        return counterPNS;
+    }
+
+    @Override
+    public double hitungPajak(){
+        return 0.10 * getPendapatan();
+    }
+
+    @Override
+    public int hitungMasaKerja(){
+        int A = 8;
+        return LocalDate.now().getYear() - getTglMulaiKerja().getYear() + A;
+    }
+
+    @Override
+    public void cetakInfo(){
+        super.cetakInfo();
+        System.out.println("NIP: " + nip);
+    }
+}
---
+++ Praktikum 05/Tugas/src/Petani.java
@@ -0,0 +1,41 @@
+import java.time.LocalDate;
+
+public class Petani extends Manusia implements Pajak {
+    private String asal_kota;
+    private static int counterPetani = 0;
+    
+    public Petani(String nama, LocalDate tgl, String alamat, double pendapatan, String asal_kota){
+        super(nama, tgl, alamat, pendapatan);
+        this.asal_kota = asal_kota;
+        counterPetani++;
+    }
+
+    public String getAsalKota(){
+        return asal_kota;
+    }
+
+    public void setAsalKota(String asal_kota){
+        this.asal_kota = asal_kota;
+    }
+
+    public static int getCounterPetani(){
+        return counterPetani;
+    }
+
+    @Override
+    public double hitungPajak(){
+        return 0;
+    }
+
+    @Override
+    public int hitungMasaKerja(){
+        int C = 0;
+        return LocalDate.now().getYear() - getTglMulaiKerja().getYear() + C;
+    }
+
+    @Override
+    public void cetakInfo(){
+        super.cetakInfo();
+        System.out.println("Asal Kota: " + asal_kota);
+    }
+}
